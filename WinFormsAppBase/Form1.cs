571e482f831da417b076b27a7d8c164705f400e0
陳廣旂 <chi_19911231@outlook.com>
2025-05-01 15:06:57 +0800

    新增信件功能

--- WinFormsAppBase/Form1.cs
+++ WinFormsAppBase/Form1.cs
@@ -1,3 +1,7 @@
+using System.Net.Mail;
+using System.Net;
+using System.Runtime;
+
 namespace WinFormsAppBase
 {
     public partial class Form1 : Form
@@ -14,10 +18,70 @@ namespace WinFormsAppBase
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {           
+        {
+
+            //Mail();
+
             timer1.Interval = 1000;
             timer1.Start();
-        } 
+        }
+
+        public void Mail(string file) 
+        {
+
+            // 建立 SmtpClient 物件
+            SmtpClient smtp = new SmtpClient(Settings.AppConfig.Setting.MailHost, Settings.AppConfig.Setting.MailPort);
+            smtp.Credentials = new NetworkCredential(Settings.AppConfig.Setting.SendMail, Settings.AppConfig.Setting.SendPassword);
+            smtp.EnableSsl = true;
+
+            // 建立 MailMessage 物件
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress( Settings.AppConfig.Setting.SendMail?? ""); // 寄件人
+
+
+            //收件人
+            string[] receiveMailList = Settings.AppConfig.Setting.ReceiveMailList.Split(';');
+            foreach (var receiveMail in receiveMailList)
+            {
+                if (!string.IsNullOrEmpty(receiveMail))
+                {
+                   mail.To.Add(receiveMail);
+                }
+            }
+
+            //CC信件
+            string[] receiveMailCcList = Settings.AppConfig.Setting.ReceiveMailCcList.Split(';');
+            foreach (var receiveMailCc in receiveMailCcList)
+            {
+                if (!string.IsNullOrEmpty(receiveMailCc))
+                {
+                    mail.CC.Add(receiveMailCc);
+                }
+            }
+
+            mail.Subject = Settings.AppConfig.Setting.MailSubject;
+            mail.Body = Settings.AppConfig.Setting.MailBody;
+            mail.Attachments.Add(new Attachment($@"{file}"));
+
+            //mail.Attachments.Add(new Attachment($@"D:\\Test\\新文字文件123.txt"));
+            //mail.Attachments.Add(new Attachment($@"D:\\Test\\新文字文件456.txt"));
+            //mail.Attachments.Add(new Attachment($@"D:\\Test\\新文字文件789.txt"));
+
+
+
+
+            try
+            {
+                smtp.Send(mail);
+                SetLabelText("寄信成功");
+            }
+            catch (Exception ex)
+            {              
+                SetLabelText(ex.Message);
+            }
+
+        }
+
 
         private async void timer1_Tick(object sender, EventArgs e)
         {
@@ -46,9 +110,17 @@ namespace WinFormsAppBase
                 DateTime creationTime = File.GetCreationTime(file);
                 if (creationTime >= fromTime)
                 {
-                    SetLabelText($"新增檔案：{file}（建立時間：{creationTime}）");
+                    SetLabelText($"新增檔案：{file} （建立時間：{creationTime}）");
+
+
+                    Mail(file);
                 }
             }
+
+
+
+
+
         }
 
         private void SetLabelText(string text)
--- WinFormsAppBase/Settings/AppSetting.cs
+++ WinFormsAppBase/Settings/AppSetting.cs
@@ -22,6 +22,47 @@
         public int FrequencySeconds { get; set; }
 
 
+        /// <summary>
+        /// 信箱Host
+        /// </summary>
+        public string? MailHost { get; set; }
+
+
+        /// <summary>
+        /// 信箱Port
+        /// </summary>
+        public int MailPort { get; set; }
+
+        /// <summary>
+        /// 寄件者
+        /// </summary>
+        public string? SendMail { get; set; }
+        /// <summary>
+        /// 寄件密碼
+        /// </summary>
+        public string? SendPassword { get; set; }
+
+      
+
+        /// <summary>
+        /// 收件者清單
+        /// </summary>
+        public string? ReceiveMailList { get; set; }
+
+        /// <summary>
+        /// CC收件者清單
+        /// </summary>
+        public string? ReceiveMailCcList { get; set; }
+
+                
+        /// <summary>
+        /// 主旨
+        /// </summary>
+        public string? MailSubject { get; set; }
+        /// <summary>
+        /// 內容
+        /// </summary>
+        public string? MailBody { get; set; }
 
 
         /// <summary>
